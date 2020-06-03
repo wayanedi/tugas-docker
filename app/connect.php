9946521ca3161005115ffa74645bdc02fd8f66f8
wayanedi <wayan.sudarma@ti.ukdw.ac.id>
2020-06-04 02:34:41 +0700

    add port

--- app/connect.php
+++ app/connect.php
@@ -3,6 +3,7 @@
 	$user = 'root';
 	$pass = 'mypw';
 	$db = 'db-tugas5';
+	$port = '3306';
 
-	$con = mysqli_connect($server,$user,$pass,$db) or die("failed to connect");
+	$con = mysqli_connect($server,$user,$pass,$db, $port) or die("failed to connect");
 ?>
